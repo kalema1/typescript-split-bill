7dc3e12059bd727d66a1a2b6591dd1da1f2398fc
kalema1 <kalemar8@gmail.com>
2024-03-14 12:03:21 +0300

    add styles for components
    - added styles for the components, to structure the application.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import AddFriendForm from "./components/AddFriendForm";
+import FriendsList from "./components/FriendsList";
+import SplitBillForm from "./components/SplitBillForm";
+
 export default function App() {
-  return <div>Hello</div>;
+  return (
+    <div className="app-container">
+      <div className="sidebar">
+        <FriendsList />
+        <AddFriendForm />
+      </div>
+      <SplitBillForm />
+    </div>
+  );
 }
