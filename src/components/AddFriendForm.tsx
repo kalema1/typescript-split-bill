b45c0842095e6c68a740575049a6813f098c1f42
kalema1 <kalemar8@gmail.com>
2024-03-15 11:30:40 +0300

    update AddFriendForm component.
    - added more jsx in the AddFriendForm component.
    - added styles for the AdFriendForm component.

--- src/components/AddFriendForm.tsx
+++ src/components/AddFriendForm.tsx
@@ -1,5 +1,16 @@
 // component file responsible for adding new friends to the list
 
+import Button from "./Button";
+
 export default function AddFriendForm() {
-  return <div>form</div>;
+  return (
+    <form className=" form add-friend-form">
+      <label className="label">👩🏻‍🤝‍🧑🏼 Friend name</label>
+      <input type="text" />
+
+      <label className="label">📸 Image</label>
+      <input type="text" />
+      <Button onClick={() => console.log("add friend")}>Add Friend</Button>
+    </form>
+  );
 }
