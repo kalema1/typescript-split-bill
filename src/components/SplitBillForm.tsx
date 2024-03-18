4e3f7d3d77e1c083c05d7cfaaf9f8b74020ad9a0
kalema1 <kalemar8@gmail.com>
2024-03-18 13:23:57 +0300

    feat: add state for the input element in the SplitBillForm component
    - added state for the bill input element.
    - added state for the expense input element.
    - added state for the select element.
    - added state for the paid by friend element.
    - created a handleChangelPaidByUser function that updates state of the
      paid by user input element.

--- src/components/SplitBillForm.tsx
+++ src/components/SplitBillForm.tsx
@@ -1,22 +1,45 @@
 // component file responsible for spliting the bill
 
+import { useState } from "react";
 import Button from "./Button";
 
 export default function SplitBillForm({ selectedFriend }) {
+  const [bill, setBill] = useState<number | string>("");
+  const [paidByUser, setPaidByUser] = useState<number | string>("");
+  const [whoIsPaying, setWhoisPaying] = useState("user");
+
+  const paidByFriend = bill ? bill - paidByUser : "";
+
+  /**
+   * handle the change of expense value
+   */
+  function handleChangePaidByUser(event: React.ChangeEvent<HTMLInputElement>) {
+    const IsGreaterThanBill = Number(event.target.value) > bill;
+    const value = IsGreaterThanBill ? paidByUser : Number(event.target.value);
+    setPaidByUser(value);
+  }
+
   return (
     <form className="form split-bill-form">
       <h2>SPLIT A BILL WITH {selectedFriend.name}</h2>
       <label className="label">💰 Bill value</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={bill}
+        onChange={(event) => setBill(Number(event.target.value))}
+      />
 
       <label className="label">🧍‍♂️ Your expense</label>
-      <input type="text" />
+      <input type="text" value={paidByUser} onChange={handleChangePaidByUser} />
 
       <label className="label">👩🏻‍🤝‍🧑🏼 {selectedFriend.name}'s expense</label>
-      <input type="text" disabled />
+      <input type="text" disabled value={paidByFriend} />
 
       <label>🤑 Who is paying the bill</label>
-      <select>
+      <select
+        value={whoIsPaying}
+        onChange={(event) => setWhoisPaying(event.target.value)}
+      >
         <option value="user">you</option>
         <option value="friend">{selectedFriend.name}</option>
       </select>
