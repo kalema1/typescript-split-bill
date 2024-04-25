37d8809d44b96db25e0878688a17d6c2af159525
kalema1 <kalemar8@gmail.com>
2024-04-25 11:42:58 +0300

    feat: add a heading for the application
    - added a heading for the application.
    - styled the heading.

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,27 @@ import FriendsList from "./components/FriendsList";
 import SplitBillForm from "./components/SplitBillForm";
 import { FriendsContext } from "./contexts/FriendsContext";
 import { ADD_FRIEND, CLOSE } from "./constants/buttonContants";
+import Heading from "./components/Heading";
 
 export default function App() {
   const { showAddFriendForm, handleShowAddfriendForm, selectedFriend } =
     useContext(FriendsContext);
   return (
-    <div className="app-container">
-      <div className="sidebar">
-        <FriendsList />
+    <>
+      <Heading />
+      <div className="app-container">
+        <div className="sidebar">
+          <FriendsList />
 
-        {showAddFriendForm && <AddFriendForm />}
+          {showAddFriendForm && <AddFriendForm />}
 
-        <Button onClick={handleShowAddfriendForm}>
-          {showAddFriendForm ? CLOSE : ADD_FRIEND}
-        </Button>
+          <Button onClick={handleShowAddfriendForm}>
+            {showAddFriendForm ? CLOSE : ADD_FRIEND}
+          </Button>
+        </div>
+
+        {!!selectedFriend && <SplitBillForm />}
       </div>
-
-      {!!selectedFriend && <SplitBillForm />}
-    </div>
+    </>
   );
 }
---
+++ src/components/Heading.tsx
@@ -0,0 +1,3 @@
+export default function Heading() {
+  return <h1 className="heading-primary">Split a Bill With a Friend</h1>;
+}
