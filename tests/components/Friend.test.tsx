7929df11dd62f44e9d45dd0b69fee35568b75f28
kalema1 <kalemar8@gmail.com>
2024-05-02 14:38:24 +0300

    tests: add tests for the Friend component
    - added tests to check if friend item has an image.
    - added test to check the heading has the right test content.

---
+++ tests/components/Friend.test.tsx
@@ -0,0 +1,36 @@
+import { it, expect, describe } from "vitest";
+import { render, screen } from "@testing-library/react";
+import React from "react";
+
+import Friend from "../../src/components/Friend";
+import { FriendsProvider } from "../../src/contexts/FriendsContext";
+
+describe("Friend", () => {
+  const renderComponent = () => {
+    const id = crypto.randomUUID();
+    const friend = {
+      id,
+      name: "becky",
+      image: `https://i.pravatar.cc/48?u=118836?=${id}`,
+      balance: 50,
+    };
+
+    render(
+      <FriendsProvider>
+        <Friend friend={friend} />
+      </FriendsProvider>
+    );
+
+    return {
+      heading: screen.getByRole("heading"),
+      image: screen.getByAltText(friend.name),
+      friend,
+    };
+  };
+
+  it("should render a friend item", () => {
+    const { heading, image, friend } = renderComponent();
+    expect(image).toBeInTheDocument();
+    expect(heading).toHaveTextContent(friend.name);
+  });
+});
